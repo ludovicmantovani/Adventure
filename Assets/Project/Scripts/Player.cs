d00ab73c40ab41849ccb1719da81d0eedd9f635b
Ludovic <ludovic.mantov@gmail.com>
2017-12-29 13:00:31 +0100

    Add jump to the player
    Use raycasting to determine if an object is under the player. If it's true, the player can jump just one time (GetKeyDown).

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public float jumpingForce = 200f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,10 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		ProcessInput ();
+	}
+
+	void ProcessInput(){
 		if (Input.GetKey("right")) {
 			transform.position += Vector3.right * 5f * Time.deltaTime;
 		}
@@ -23,5 +29,13 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey("down")) {
 			transform.position += Vector3.back * 5f * Time.deltaTime;
 		}
+		if (Input.GetKeyDown("space") && canJump()) {
+			GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+		}
+	}
+
+	bool canJump(){
+		RaycastHit hit;
+		return(Physics.Raycast (transform.position, Vector3.down, out hit, 1.01f));
 	}
 }
