c9c207875d9c1f7d61ad43165e2504052892f9e4
Ludovic <ludovic.mantov@gmail.com>
2017-12-30 19:11:29 +0100

    Add camera logic

---
+++ Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+	public GameObject target;
+	public Vector3 offset = new Vector3 (0, 8, -3);
+	public float focusSpeed = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * focusSpeed);
+	}
+}
